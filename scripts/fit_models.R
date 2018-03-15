8cb63d84c22461608ec5cdb0c4c0359d5840fe73
Zandra Ho <zho@wm73d-118.home>
2018-03-14 21:36:42 -0400

    added scripts for benchmark models

--- scripts/fit_models.R
+++ scripts/fit_models.R
@@ -9,17 +9,102 @@ library(readr)
 library(stringr)
 library(dependr)
 
-source('~/Documents/demeter2/scripts/pull_data.R')
+source('~/Documents/demeter2/scripts/setup_directory_system.R')
+
+
+######################
+# Shared Functions
+######################
+
+# Stores functions for annotating CCLE cancer cell lines
+load('~/Documents/DRIVE ATARiS Validation/Annotations.RData')
+
+# Load LFC Matrices, stored on Taiga
+
+get_LFC_matrices <- function() {
+  # Achilles
+  a98k_LFC_mat <- load.from.taiga(data.name='achilles-98k-repcollapsed-lfc-19ce', data.version=1)
+  a55k1_LFC_mat <- load.from.taiga(data.name='achilles-55k-batch1-repcollapsed-lfc-d708', data.version=1)
+  a55k2_LFC_mat <- load.from.taiga(data.name='achilles-55k-batch2-repcollapsed-lfc-bd7f', data.version=1)
+  # DRIVE
+  BGPD_LFC_mat <- load.from.taiga(data.name='drive-lfc-matrices-3867', data.version=9, data.file='BGPD_LFC_mat')
+  poolA_LFC_mat <- load.from.taiga(data.name='drive-lfc-matrices-3867', data.version=9, data.file='poolA_LFC_mat')
+  poolB_LFC_mat <- load.from.taiga(data.name='drive-lfc-matrices-3867', data.version=9, data.file='poolB_LFC_mat')
+  # Marcotte
+  Marcotte_LFC_mat <- load.from.taiga(data.name='marcotte-demeter-z-scored-expanded-gene-sols-e905', data.version=10, data.file='log.effect.removed.bad')
+  # Convert cleannames to CCLE IDs
+  colnames(Marcotte_LFC_mat) <- ifelse(is.na(CleanCellLineName(colnames(Marcotte_LFC_mat))), 
+                                       colnames(Marcotte_LFC_mat), 
+                                       CleanCellLineName(colnames(Marcotte_LFC_mat)))
+  Achilles_LFC_mats <<- list(a98k_LFC_mat, a55k1_LFC_mat, a55k2_LFC_mat)
+  DRIVE_LFC_mats <<- list(BGPD_LFC_mat, poolA_LFC_mat, poolB_LFC_mat)
+  Marcotte_LFC_mat <<- Marcotte_LFC_mat
+  
+}
+
+# Find sequences that target too many genes (>10) and filter them out
+remove_promiscuous_seqs <- function(map, datatype) {
+  if (datatype == 'Achilles' || datatype == 'DRIVE') {
+    promiscuous_seqs <- map %>% 
+      dplyr::select(seq, Gene = Entrez_ID)
+  } else if (datatype == 'Marcotte') {
+    promiscuous_seqs <- map %>% 
+      dplyr::select(seq, Gene = Gene_Entrez_ID)
+  }
+  promiscuous_seqs %<>% 
+    unique() %>% 
+    group_by(seq) %>% 
+    dplyr::summarise(num_genes_targeted = n()) %>% 
+    filter(num_genes_targeted > 10) %>% 
+    .[['seq']]
+  
+  clean_map <- map[!(map$seq %in% promiscuous_seqs),]
+  print(paste0('Removed ', nrow(map) - nrow(clean_map), ' promiscuous sequences from ', datatype, ' hairpin map'))
+  return(clean_map)
+}
+
+# Remove outdated genes to create hairpin map
+get_hairpin_map <- function() {
+  map <- load.from.taiga(data.name='gpp-shrna-mapping-8759', data.version=1, data.file='CP1175_20171102_19mer')
+  map %<>% dplyr::select(Gene = `Gene Symbol`, Entrez_ID = `Gene ID`, seq = `Barcode Sequence`)
+  map <- map[-(grep('NO_CUR', map$Gene)),] # filter out any genes containing "NO_CURRENT..."
+  return(remove_promiscuous_seqs(map, 'Achilles'))
+}
+
+# Make sure X doesn't get appended to beginning of column names
+check_CCLE_colnames <- function(mat) {
+  stopifnot(length(grep('^X[0-9]', colnames(mat))) == 0)
+}
+
+# Normalizes LFC matrix by CL and changes rownames to reflect the sequence, entrez id, and batch name
+process_LFC <- function(LFC, datatype, batch_name, distinct_hp) {
+  LFC %<>% scale(center = TRUE, scale = TRUE) # zscore normalize by CL
+  cur <- hairpin_map %>% filter(seq %in% rownames(LFC)) %>% dplyr::select(seq, Gene, Entrez_ID) %>% unique()
+  new_LFC <- LFC[cur$seq, ] # Split into separate rows for hairpins targeting each gene
+  if (distinct_hp) {
+    # hairpins from different batches should be treated as distinct
+    rownames(new_LFC) <- paste0(cur$seq, '@', cur$Gene, '@', cur$Entrez_ID, '@', batch_name)
+  } else {
+    # hairpins from different batches should be treated as the same
+    rownames(new_LFC) <- paste0(cur$seq, '@', cur$Gene, '@', cur$Entrez_ID)
+  }
+  return(new_LFC)
+}
+
+
+##############
+# Fit Models
+##############
 
 # Retrieve data for all models
-get_hairpin_map()
-get_LFC_matrices()
-Achilles_LFC_mats <- list(a98k_LFC_mat, a55k1_LFC_mat, a55k2_LFC_mat)
-DRIVE_LFC_mats <- list(BGPD_LFC_mat, poolA_LFC_mat, poolB_LFC_mat)
 
-# Fit each model on each datatype
+get_LFC_matrices() # Matrices are returned globally
+hairpin_map <- get_hairpin_map()
 
-############################################## Gene Averaging ##############################################
+
+###################
+# Gene Averaging
+###################
 
 preprocess_for_GA <- function(LFC) {
   LFC %<>% scale(center = TRUE, scale = TRUE) # zscore normalize by CL
@@ -29,7 +114,7 @@ preprocess_for_GA <- function(LFC) {
     melt(id.vars = 'seq', variable.name = 'CL', value.name = 'LFC')
   LFC %<>% left_join(hairpin_map, by = 'seq')
   LFC$Gene_Entrez_ID <- paste0(LFC$Gene, ' (', LFC$Entrez_ID, ')')
-  return(LFC %>% select(CL, LFC, Gene_Entrez_ID))
+  return(LFC %>% dplyr::select(CL, LFC, Gene_Entrez_ID))
 }
 
 fit_GA <- function(LFC, fname) {
@@ -48,9 +133,9 @@ GA_wrapper <- function(datatype, fname, LFC_mats) {
   fit_GA(LFC, fname)
 }
 
-GA_wrapper('Achilles', Achilles_GA_fname, Achilles_LFC_mats)
-GA_wrapper('DRIVE', DRIVE_GA_fname, DRIVE_LFC_mats)
-GA_wrapper('Marcotte', Marcotte_GA_fname, list(Marcotte_LFC_mat))
+GA_wrapper('Achilles', paths$Achilles_GA_fname, Achilles_LFC_mats)
+GA_wrapper('DRIVE', paths$DRIVE_GA_fname, DRIVE_LFC_mats)
+GA_wrapper('Marcotte', paths$Marcotte_GA_fname, list(Marcotte_LFC_mat))
 
 #### Achilles + DRIVE + Marcotte GA takes lots of memory to run. We ran this remotely.
 #### Our pipeline involves uploading an intermeditate RDS file to Google Cloud, as shown below.
@@ -61,9 +146,9 @@ GA_wrapper('Marcotte', Marcotte_GA_fname, list(Marcotte_LFC_mat))
 #   LFC_mats <- list(a98k_LFC_mat, a55k1_LFC_mat, a55k2_LFC_mat, BGPD_LFC_mat, poolA_LFC_mat, poolB_LFC_mat, Marcotte_LFC_mat)
 #   LFC <- mapply(preprocess_for_GA, LFC = LFC_mats, SIMPLIFY = FALSE)
 #   LFC <- do.call(rbind, LFC)
-#   write_rds(LFC, 'Achilles_DRIVE_Marcotte_GA_mat.RDS')
+#   write_rds(LFC, paths$Combined_RDS_fname)
 # }
-# 
+#
 # fit_GA <- function(LFC, fname) {
 #   LFC %<>% mutate(CL = as.character(CL))
 #   # Convert to CCLE names
@@ -74,38 +159,17 @@ GA_wrapper('Marcotte', Marcotte_GA_fname, list(Marcotte_LFC_mat))
 #   # ensure >1 hairpin/gene
 #   GA <- GA[GA$num_shRNAs > 1,]
 #   GA_mat <- GA %>% acast(Gene_Entrez_ID ~ CL, value.var = 'avg_score')
-#   
+#
 #   write.csv(GA_mat, fname)
 # }
-# 
+#
 # GA_combination()
-# LFC <- read_rds('Achilles_DRIVE_Marcotte_GA_mat.RDS')
-# fit_GA(LFC, 'Achilles_DRIVE_Marcotte_GA_scores.csv')
+# LFC <- read_rds(paths$Combined_RDS_fname)
+# fit_GA(LFC, paths$Combined_GA_fname)
 
-############################################## Shared Functions ##############################################
-
-# Make sure X doesn't get appended to beginning of column names
-check_CCLE_colnames <- function(mat) {
-  stopifnot(length(grep('^X[0-9]', colnames(mat))) == 0)
-}
-
-# Normalizes LFC matrix by CL and changes rownames to reflect the sequence, entrez id, and batch name
-process_LFC <- function(LFC, datatype, batch_name, distinct_hp) {
-  LFC %<>% scale(center = TRUE, scale = TRUE) # zscore normalize by CL
-  # hairpin_map %<>% mutate(Gene_Entrez_ID = paste0(Gene, '_', Entrez_ID))
-  cur <- hairpin_map %>% filter(seq %in% rownames(LFC)) %>% select(seq, Gene, Entrez_ID) %>% unique()
-  new_LFC <- LFC[cur$seq, ] # Split into separate rows for hairpins targeting each gene
-  if (distinct_hp) {
-    # hairpins from different batches should be treated as distinct
-    rownames(new_LFC) <- paste0(cur$seq, '@', cur$Gene, '@', cur$Entrez_ID, '@', batch_name)
-  } else {
-    # hairpins from different batches should be treated as the same
-    rownames(new_LFC) <- paste0(cur$seq, '@', cur$Gene, '@', cur$Entrez_ID)
-  }
-  return(new_LFC)
-}
-
-############################################## RSA ##############################################
+##########
+# RSA
+##########
 
 # Create a file for each cell line, run RSA for each cell line, and stitch outputs together
 preprocess_for_RSA <- function(LFC_mats, dir) {
@@ -118,13 +182,13 @@ preprocess_for_RSA <- function(LFC_mats, dir) {
   # Use "@" as a delimiter
   long %<>% left_join(hairpin_map, by = 'seq') %>%
     mutate(Entrez_ID_CL = paste0(Entrez_ID, '@', CL)) %>% 
-    select(CL, Gene_ID = Entrez_ID_CL, Well_ID = seq, Score)
+    dplyr::select(CL, Gene_ID = Entrez_ID_CL, Well_ID = seq, Score)
   long %>% d_ply(.(CL), function(df) {
     print(df$CL %>% unique())
     # Filter out genes targeted by only one hairpin
     gene_count <- table(df$Gene_ID)
     good_genes <- gene_count[gene_count > 1] %>% names()
-    temp <- df %>% filter(Gene_ID %in% good_genes) %>% select(Gene_ID, Well_ID, Score)
+    temp <- df %>% filter(Gene_ID %in% good_genes) %>% dplyr::select(Gene_ID, Well_ID, Score)
     write.csv(temp, paste0(dir, unique(df$CL), '.csv'), row.names = F)
   })
 }
@@ -137,7 +201,7 @@ postprocess_RSA_output <- function(dir, fname) {
       dplyr::select(CL, Entrez_ID, Score = LogP) %>%
       left_join(hairpin_map, by = 'Entrez_ID') %>%
       mutate(Gene_Entrez_ID = paste0(Gene, ' (', Entrez_ID, ')')) %>%
-      select(CL, Gene_Entrez_ID, Score) %>% unique()
+      dplyr::select(CL, Gene_Entrez_ID, Score) %>% unique()
     return(new)
   })
   RSA <- do.call(rbind, RSA)
@@ -145,16 +209,18 @@ postprocess_RSA_output <- function(dir, fname) {
   write.csv(mat, fname)
 }
 
-preprocess_for_RSA(DRIVE_LFC_mats, DRIVE_RSA_inputs_directory)
-preprocess_for_RSA(Achilles_LFC_mats, Achilles_RSA_inputs_directory)
+preprocess_for_RSA(DRIVE_LFC_mats, paths$DRIVE_RSA_inputs_directory)
+preprocess_for_RSA(Achilles_LFC_mats, paths$Achilles_RSA_inputs_directory)
 
 #### INTERMEDIATE STEP: Use scripts in run_RSA to generate RSA scores for each cell line ####
 
-postprocess_RSA_output(DRIVE_RSA_outputs_directory, DRIVE_RSA_scores_fname)
-postprocess_RSA_output(Achilles_RSA_outputs_directory, Achilles_RSA_scores_fname)
+postprocess_RSA_output(DRIVE_RSA_outputs_directory, paths$DRIVE_RSA_scores_fname)
+postprocess_RSA_output(Achilles_RSA_outputs_directory, paths$Achilles_RSA_scores_fname)
 
 
-############################################## ATARiS ##############################################
+###########
+# ATARiS
+###########
 
 write_ATARiS_gct <- function(df, gct_fname) {
   gene_entrez_ids <- rownames(df) %>% llply(function(str) {
@@ -172,16 +238,16 @@ preprocess_for_ATARiS <- function(LFC_mats, datatype, batch_names) {
   if (datatype == 'DRIVE') {
     LFC <- melt(mats[[1]], value.name = 'LFC') %>% rbind(melt(mats[[2]], value.name = 'LFC')) %>% rbind(melt(mats[[3]], value.name = 'LFC')) %>% mutate(Var1 = as.character(Var1))
     drive_df <- acast(LFC, Var1 ~ Var2, value.var = 'LFC')
-    write_ATARiS_gct(drive_df, DRIVE_preATARiS_fname) 
+    write_ATARiS_gct(drive_df, paths$DRIVE_preATARiS_fname) 
   } else if (datatype == 'Achilles') {
     # Achilles 98k batch
     a98k_LFC <- melt(mats[[1]], value.name = 'LFC')
     a98k_df <- acast(a98k_LFC, Var1 ~ Var2, value.var = 'LFC')
-    write_ATARiS_gct(a98k_df, Achilles_98k_preATARiS_fname)
+    write_ATARiS_gct(a98k_df, paths$Achilles_98k_preATARiS_fname)
     # Achilles 55k batches
     a55k_LFC <- melt(mats[[2]], value.name = 'LFC') %>% rbind(melt(mats[[3]], value.name = 'LFC'))
     a55k_df <- acast(a55k_LFC, Var1 ~ Var2, value.var = 'LFC')
-    write_ATARiS_gct(a55k_df, Achilles_55k_preATARiS_fname)
+    write_ATARiS_gct(a55k_df, paths$Achilles_55k_preATARiS_fname)
   }
 }
 
@@ -216,21 +282,23 @@ preprocess_for_ATARiS(DRIVE_LFC_mats, 'DRIVE', DRIVE_batch_names)
 
 #### INTERMEDIATE STEP: Run ATARiS on Gene Pattern ####
 
-postprocess_ATARiS_output(c(paste0(Achilles_ATARiS_directory, 'Achilles_55k.Gs.gct'), paste0(Achilles_ATARiS_directory, 'Achilles_98k.Gs.gct')), Achilles_ATARiS_scores_fname, TRUE)
-postprocess_ATARiS_output(paste0(DRIVE_ATARiS_directory, 'DRIVE.Gs.gct'), DRIVE_ATARiS_scores_fname, FALSE)
+postprocess_ATARiS_output(paste0(paths$Achilles_ATARiS_directory, c('Achilles_55k.Gs.gct', 'Achilles_98k.Gs.gct')), paths$Achilles_ATARiS_scores_fname, TRUE)
+postprocess_ATARiS_output(paste0(paths$DRIVE_ATARiS_directory, 'DRIVE.Gs.gct'), paths$DRIVE_ATARiS_scores_fname, FALSE)
 
-############################################## DEMETER 1 ##############################################
+############
+# DEMETER 1 
+############
 
 # Create batch files for input to DEMETER1
 make_batches_file <- function(LFC_mats, datatype) {
   CL <- c(colnames(LFC_mats[[1]]), colnames(LFC_mats[[2]]), colnames(LFC_mats[[3]]))
   if (datatype == 'Achilles') {
     batches <- data.frame(Name = CL, `DEMETER batch` = c(rep(1, ncol(LFC_mats[[1]])), rep(2, ncol(LFC_mats[[2]])), rep(3, ncol(LFC_mats[[3]]))), check.names = FALSE)
-    fname <- Achilles_DEMETER1_batches_fname
+    fname <- paths$Achilles_DEMETER1_batches_fname
   } else if (datatype == 'DRIVE') {
     # All CLs in DRIVE data will be considered to be in the same batch
     batches <- data.frame(Name = CL, `DEMETER batch` = rep(1, length(CL)), check.names = FALSE)
-    fname <- DRIVE_DEMETER1_batches_fname
+    fname <- paths$DRIVE_DEMETER1_batches_fname
   }
   write.csv(batches, fname, row.names = FALSE)
 }
@@ -280,8 +348,8 @@ postprocess_DEMETER1_output <- function(input_fname, output_fname) {
   write.csv(df, output_fname)
 }
 
-preprocess_for_DEMETER1(Achilles_LFC_mats, 'Achilles', Achilles_batch_names, Achilles_DEMETER1_mat_fnames)
-preprocess_for_DEMETER1(DRIVE_LFC_mats, 'DRIVE', DRIVE_batch_names, DRIVE_DEMETER1_mat_fname)
+preprocess_for_DEMETER1(Achilles_LFC_mats, 'Achilles', Achilles_batch_names, paths$Achilles_DEMETER1_mat_fnames)
+preprocess_for_DEMETER1(DRIVE_LFC_mats, 'DRIVE', DRIVE_batch_names, paths$DRIVE_DEMETER1_mat_fname)
 
 #### INTERMEDIATE STEP: Run DEMETER on the cloud with output of preprocessing step (see run_DEMETER1 for more details) ####
 
@@ -289,7 +357,7 @@ preprocess_for_DEMETER1(DRIVE_LFC_mats, 'DRIVE', DRIVE_batch_names, DRIVE_DEMETE
 DRIVE_DEMETER1_output_fname <- '~/Desktop/rnai_pipeline/DRIVE/DEMETER1/output/GeneSols.csv'
 Achilles_DEMETER1_output_fname <- '~/Desktop/rnai_pipeline/Achilles/DEMETER1/output/GeneSolsCleaned.csv'
 
-postprocess_DEMETER1_output(DRIVE_DEMETER1_output_fname, DRIVE_DEMETER1_scores_fname)
-postprocess_DEMETER1_output(Achilles_DEMETER1_output_fname, Achilles_DEMETER1_scores_fname)
+postprocess_DEMETER1_output(DRIVE_DEMETER1_output_fname, paths$DRIVE_DEMETER1_scores_fname)
+postprocess_DEMETER1_output(Achilles_DEMETER1_output_fname, paths$Achilles_DEMETER1_scores_fname)
 
 
