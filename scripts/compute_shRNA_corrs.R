b54924929b13c2269e008189159aa0e7b8be9de1
James McFarland <jmmcfarl@broadinstitute.org>
2018-04-24 15:09:35 -0400

    update shRNA stats calculation function

--- scripts/compute_shRNA_corrs.R
+++ scripts/compute_shRNA_corrs.R
@@ -12,21 +12,30 @@ library(ggplot2)
 library(doMC)
 doMC::registerDoMC(cores = 4)
 
+new_name_table <- read.csv('~/CPDS/demeter2/results/name_change_map.csv', check.names = FALSE, stringsAsFactors = FALSE)
+new_name_map <- new_name_table$new_name %>% set_names(new_name_table$old_name)
+
 sh_targets <- load.from.taiga(data.name = 'gpp-shrna-mapping-8759', 
                               data.version = 2, 
                               data.file = 'shmap_19mer_noXLOC') 
 DRIVE_D2 <- load.from.taiga(data.name='demeter2-drive-0591', data.version=8, data.file='gene_means_proc')
+DRIVE_D2_hp <- load.from.taiga(data.name='demeter2-drive-0591', data.version=8, data.file='hp_data_comb') %>% 
+  as.data.frame() %>% 
+  rownames_to_column(var = 'hp')
 DRIVE_used_genes <- str_match(rownames(DRIVE_D2), '\\((.+)\\)')[,2]
 
 DRIVE_mat_BGPD <- load.from.taiga(data.name = 'drive-lfc-matrices-3867',
                                data.version = 9,
                                data.file = 'BGPD_LFC_mat') %>% scale(center = T, scale = T)
+colnames(DRIVE_mat_BGPD) %<>% plyr::revalue(new_name_map)
 DRIVE_mat_poolA <- load.from.taiga(data.name = 'drive-lfc-matrices-3867',
                                   data.version = 9,
                                   data.file = 'poolA_LFC_mat') %>% scale(center = T, scale = T)
+colnames(DRIVE_mat_poolA) %<>% plyr::revalue(new_name_map)
 DRIVE_mat_poolB <- load.from.taiga(data.name = 'drive-lfc-matrices-3867',
                                    data.version = 9,
                                    data.file = 'poolB_LFC_mat') %>% scale(center = T, scale = T)
+colnames(DRIVE_mat_poolB) %<>% plyr::revalue(new_name_map)
 
 all_hps <- rownames(DRIVE_mat_BGPD) %>% 
   union(rownames(DRIVE_mat_poolA)) %>% 
@@ -67,7 +76,7 @@ DRIVE_merged %<>% t()
 sh_targets %<>% 
   filter(`Barcode Sequence` %in% all_hps, 
          !grepl('NO_CURRENT', `Gene ID`), 
-         `Gene ID` %in% DRIVE_used_genes,
+         # `Gene ID` %in% DRIVE_used_genes,
           grepl('[0-9]', `Gene ID`))
 
 all_genes <- sh_targets %>% 
@@ -75,19 +84,38 @@ all_genes <- sh_targets %>%
   dplyr::distinct(`Gene ID`, .keep_all=T)
 
 res <- ddply(all_genes, 1, function(grow) {
+  min_pairs <- 10
   target_gene <- grow[['Gene ID']]
+  # print(grow[['Gene Symbol']])
   cur_seqs <- sh_targets %>% 
     filter(`Gene ID` == target_gene) %>% 
     .[['Barcode Sequence']]
   
-  temp <- cor(DRIVE_merged[,cur_seqs], use = 'pairwise.complete.obs')
-  diag(temp) <- NA
-  cor_stats <- adply(temp, 1, function(row) {
-    data.frame(
-      best_cor = max(row, na.rm=T),
-      up_quant_cor = quantile(row, 0.75, na.rm=T)
-    )
-  }, .id = 'hp')
+  n_pairs <- t(!is.na(DRIVE_merged[,cur_seqs])) %*% (!is.na(DRIVE_merged[,cur_seqs])) # same as count.pairwise(x,y) from psych package
+  
+  if (length(cur_seqs) >= 2) {
+    temp <- cor(DRIVE_merged[,cur_seqs], use = 'pairwise.complete.obs')
+    diag(temp) <- NA
+    temp[n_pairs < min_pairs] <- NA
+    cor_stats <- adply(temp, 1, function(row) {
+      data.frame(
+        best_cor = max(row, na.rm=T),
+        up_quant_cor = quantile(row, 0.75, na.rm=T)
+      )
+    }, .id = 'hp') %>% 
+      mutate(best_cor = ifelse(is.infinite(best_cor), NA, best_cor))
+  } else {
+    cor_stats <- data.frame(hp = cur_seqs, best_cor = NA, up_quant_cor = NA)
+  }
+  
+  cur_title <- paste0(grow[['Gene Symbol']], ' (', target_gene, ')')
+  if (cur_title %in% rownames(DRIVE_D2)) {
+    cur_GS <- DRIVE_D2[cur_title, rownames(DRIVE_merged)]
+    cor_stats$GS_cor <- cor(DRIVE_merged[,cur_seqs], cur_GS, use = 'pairwise.complete.obs')[,1]
+  } else {
+    cor_stats$GS_cor <- NA
+  }
+  
   cor_stats %<>% mutate(`Gene ID` = grow[['Gene ID']],
                         `Gene Symbol` = grow[['Gene Symbol']])
   return(cor_stats)
@@ -95,6 +123,13 @@ res <- ddply(all_genes, 1, function(grow) {
 
 other_data <- read.csv('~/CPDS/demeter2/kube_results/DRIVE_final/1/hp_R2_post.csv', stringsAsFactors = F, check.names = F)
 comb <- full_join(res, other_data, by = 'hp')
+comb %<>% left_join(sh_targets %>% 
+                     dplyr::select(hp = `Barcode Sequence`, `Gene ID`) %>% 
+                     filter(hp %in% colnames(DRIVE_merged)) %>% 
+                     group_by(hp) %>% 
+                     summarise(n_targs = n()), by = 'hp')
+comb %<>% left_join(DRIVE_D2_hp %>% dplyr::select(hp, Geff, Seff), by = 'hp')
+comb %>% write.csv('~/CPDS/demeter2/results/DRIVE_hp_stats.csv', row.names=F)
 
 
 ######## Now for Achilles data
@@ -102,6 +137,9 @@ sh_targets <- load.from.taiga(data.name = 'gpp-shrna-mapping-8759',
                               data.version = 2, 
                               data.file = 'shmap_19mer_noXLOC') 
 Ach_D2 <- load.from.taiga(data.name='demeter2-achilles-5386', data.version=10, data.file='gene_means_proc')
+Ach_D2_hp <- load.from.taiga(data.name='demeter2-achilles-5386', data.version=10, data.file='hp_data_comb') %>% 
+  as.data.frame() %>% 
+  rownames_to_column(var = 'hp')
 Ach_used_genes <- str_match(rownames(Ach_D2), '\\((.+)\\)')[,2]
 
 Ach_mat_98k <- load.from.taiga(data.name = 'achilles-98k-repcollapsed-lfc-19ce', 
@@ -150,7 +188,7 @@ Ach_merged %<>% t()
 sh_targets %<>% 
   filter(`Barcode Sequence` %in% all_hps, 
          !grepl('NO_CURRENT', `Gene ID`), 
-         `Gene ID` %in% Ach_used_genes,
+         # `Gene ID` %in% Ach_used_genes,
          grepl('[0-9]', `Gene ID`))
 
 all_genes <- sh_targets %>% 
@@ -163,14 +201,32 @@ res <- ddply(all_genes, 1, function(grow) {
     filter(`Gene ID` == target_gene) %>% 
     .[['Barcode Sequence']]
   
-  temp <- cor(Ach_merged[,cur_seqs], use = 'pairwise.complete.obs')
-  diag(temp) <- NA
-  cor_stats <- adply(temp, 1, function(row) {
-    data.frame(
-      best_cor = max(row, na.rm=T),
-      up_quant_cor = quantile(row, 0.75, na.rm=T)
-    )
-  }, .id = 'hp')
+  n_pairs <- t(!is.na(Ach_merged[,cur_seqs])) %*% (!is.na(Ach_merged[,cur_seqs])) # same as count.pairwise(x,y) from psych package
+  
+  if (length(cur_seqs) >= 2) {
+    temp <- cor(Ach_merged[,cur_seqs], use = 'pairwise.complete.obs')
+    diag(temp) <- NA
+    temp[n_pairs < min_pairs] <- NA
+  
+      cor_stats <- adply(temp, 1, function(row) {
+      data.frame(
+        best_cor = max(row, na.rm=T),
+        up_quant_cor = quantile(row, 0.75, na.rm=T)
+      )
+    }, .id = 'hp') %>% 
+      mutate(best_cor = ifelse(is.infinite(best_cor), NA, best_cor))
+  } else {
+    cor_stats <- data.frame(hp = cur_seqs, best_cor = NA, up_quant_cor = NA)
+  }
+  
+  cur_title <- paste0(grow[['Gene Symbol']], ' (', target_gene, ')')
+  if (cur_title %in% rownames(Ach_D2)) {
+    cur_GS <- Ach_D2[cur_title, rownames(Ach_merged)]
+    cor_stats$GS_cor <- cor(Ach_merged[,cur_seqs], cur_GS, use = 'pairwise.complete.obs')[,1]
+  } else {
+    cor_stats$GS_cor <- NA
+  }
+    
   cor_stats %<>% mutate(`Gene ID` = grow[['Gene ID']],
                         `Gene Symbol` = grow[['Gene Symbol']])
   return(cor_stats)
@@ -178,5 +234,12 @@ res <- ddply(all_genes, 1, function(grow) {
 
 other_data <- read.csv('~/CPDS/demeter2/kube_results/Ach_final/1/hp_R2_post.csv', stringsAsFactors = F, check.names = F)
 comb <- full_join(res, other_data, by = 'hp')
+comb %<>% left_join(sh_targets %>% 
+                      dplyr::select(hp = `Barcode Sequence`, `Gene ID`) %>% 
+                      filter(hp %in% colnames(Ach_merged)) %>% 
+                      group_by(hp) %>% 
+                      summarise(n_targs = n()), by = 'hp')
+comb %<>% left_join(Ach_D2_hp %>% dplyr::select(hp, Geff, Seff), by = 'hp')
+comb %>% write.csv('~/CPDS/demeter2/results/Ach_hp_stats.csv', row.names=F)
 
 
